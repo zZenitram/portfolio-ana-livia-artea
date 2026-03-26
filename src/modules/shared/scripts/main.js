9c0e4915a91b8fdc420d1f26b4486a2fe0742a15
Lucas Martinez <lucas.martinez799@gmail.com>
2026-03-26 20:22:49 -0300

    refactor: Separado seção sobre mim da seção de empresas

--- src/modules/shared/scripts/main.js
+++ src/modules/shared/scripts/main.js
@@ -1,3 +1,4 @@
 import '../../header-component/scripts/header-component.js';
 import '../../home-component/scripts/home-component.js';
 import '../../brands-component/scripts/brands-component.js';
+import '../../about-me-component/scripts/about-me-component.js';
