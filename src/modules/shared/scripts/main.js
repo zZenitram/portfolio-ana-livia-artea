23239711e0f1302c6645b8940a6fd1c0d0b35977
Lucas Martinez <lucas.martinez799@gmail.com>
2026-03-26 21:38:39 -0300

    feat: Importação do "script" principal do componente de contatos

--- src/modules/shared/scripts/main.js
+++ src/modules/shared/scripts/main.js
@@ -2,3 +2,4 @@ import '../../header-component/scripts/header-component.js';
 import '../../home-component/scripts/home-component.js';
 import '../../brands-component/scripts/brands-component.js';
 import '../../about-me-component/scripts/about-me-component.js';
+import '../../contact-me-component/scripts/contact-me-component.js';  
