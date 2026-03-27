94caec217e054497c2b60caa3696deb3fbfede65
Lucas Martinez <lucas.martinez799@gmail.com>
2026-03-26 21:40:21 -0300

    feat: "Script" de inicialização do "template" do componente de contatos

---
+++ src/modules/contact-me-component/scripts/contact-me-template.js
@@ -0,0 +1,9 @@
+export class BrandsTemplate {
+    static async load() {
+        const response = await fetch('src/modules/contact-me-component/template.html');
+
+        if (!response.ok) throw new Error(`Failed to fetch HTML: ${response.status}`);
+
+        return response.text();
+    }
+}
